1207b04e883113729b6616c5a4f162d1e544cd0d
Ivax7 <xserrano2001@gmail.com>
2025-10-14 18:54:20 +0200

    Environment configuration front-back-DB

--- back-end/src/app.module.ts
+++ back-end/src/app.module.ts
@@ -1,9 +1,23 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UserModule } from './user/user.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5433,
+      username: 'postgres',
+      password: 'root',
+      database: 'TFM-GT',
+      autoLoadEntities: true,
+      synchronize: true, // solo en desarrollo
+    }),
+    UserModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
