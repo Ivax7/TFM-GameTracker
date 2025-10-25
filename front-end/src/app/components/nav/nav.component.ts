b71ab6f6b0eef5d34ba9fa6028fe6640ed3860a1
Ivax7 <xserrano2001@gmail.com>
2025-10-25 09:40:01 +0200

    user dropdown && responsive nav

--- front-end/src/app/components/nav/nav.component.ts
+++ front-end/src/app/components/nav/nav.component.ts
@@ -1,5 +1,5 @@
-import { Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
+import { Component, OnInit, HostListener, ElementRef } from '@angular/core';
+import { Router, RouterLink } from '@angular/router';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { AuthModalComponent } from '../../auth/auth-modal/auth-modal.component';
@@ -8,19 +8,24 @@ import { AuthService } from '../../auth/auth.service';
 @Component({
   selector: 'app-nav',
   standalone: true,
-  imports: [CommonModule, FormsModule, AuthModalComponent],
+  imports: [CommonModule, FormsModule, AuthModalComponent, RouterLink],
   templateUrl: './nav.component.html',
   styleUrls: ['./nav.component.css']
 })
 export class NavComponent implements OnInit {
   query = '';
   showAuthModal = false;
-  currentUser: string | null = null;
+  currentUser: any = null;
+  isDropdownOpen = false;
 
-  constructor(private router: Router, private authService: AuthService) {}
+  constructor(
+    private router: Router,
+    private auth: AuthService,
+    private elementRef: ElementRef
+  ) {}
 
   ngOnInit() {
-    this.authService.currentUser$.subscribe(user => {
+    this.auth.currentUser$.subscribe(user => {
       this.currentUser = user;
       console.log(this.currentUser)
     });
@@ -36,6 +41,24 @@ export class NavComponent implements OnInit {
   }
 
   logout() {
-    this.authService.logout();
+    this.auth.logout();
   }
+
+  toggleDropdown() {
+    this.isDropdownOpen = !this.isDropdownOpen;
+  }
+
+  // 🧠 Este decorador escucha clicks en todo el documento
+  @HostListener('document:click', ['$event'])
+  onClickOutside(event: MouseEvent) {
+    const dropdown = this.elementRef.nativeElement.querySelector('.dropdown');
+    const clickedInsideDropdown = dropdown?.contains(event.target);
+
+    if (this.isDropdownOpen && !clickedInsideDropdown) {
+      this.isDropdownOpen = false;
+    }
+  }
+
+  
+
 }
