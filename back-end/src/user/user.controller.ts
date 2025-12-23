51b2e307be3d77f9f4f6bf4f186d89fd216740c5
Ivax7 <xserrano2001@gmail.com>
2025-12-23 10:59:24 +0100

    fixing errors

--- back-end/src/user/user.controller.ts
+++ back-end/src/user/user.controller.ts
@@ -1,4 +1,5 @@
 import { Controller, Get, Patch, Delete, Body, Req, UseGuards, UnauthorizedException, UseInterceptors, UploadedFile, Query, Param } from '@nestjs/common';
+import { File } from 'multer';
 import { UserService } from './user.service';
 import { JwtAuthGuard } from '../auth/jwt-auth.guard';
 import { FileInterceptor } from '@nestjs/platform-express';
@@ -62,7 +63,8 @@ export class UserController {
   @Patch('profile')
   async updateProfile(
     @Req() req,
-    @UploadedFile() file: Express.Multer.File,
+  @UploadedFile() file: File
+    // @UploadedFile() file: Express.Multer.File,
     @Body() body: any,
   ) {
     if (!req.user) throw new UnauthorizedException('User not authenticated');
