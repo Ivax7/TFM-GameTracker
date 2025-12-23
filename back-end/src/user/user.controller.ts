74d7ee685dd2df8c5db96974c70b1f5c104e4afe
Ivax7 <xserrano2001@gmail.com>
2025-12-23 13:08:03 +0100

    Import Express

--- back-end/src/user/user.controller.ts
+++ back-end/src/user/user.controller.ts
@@ -1,5 +1,6 @@
 import { Controller, Get, Patch, Delete, Body, Req, UseGuards, UnauthorizedException, UseInterceptors, UploadedFile, Query, Param } from '@nestjs/common';
 // import { File } from 'multer';
+import { Express } from 'express';
 import { UserService } from './user.service';
 import { JwtAuthGuard } from '../auth/jwt-auth.guard';
 import { FileInterceptor } from '@nestjs/platform-express';
@@ -63,7 +64,7 @@ export class UserController {
   @Patch('profile')
   async updateProfile(
     @Req() req,
-  // @UploadedFile() file: File,
+    // @UploadedFile() file: File,
     @UploadedFile() file: Express.Multer.File,
     @Body() body: any,
   ) {
